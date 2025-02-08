fcd361517ad24e128b3655705c501483d8772a25
Harishteja <harishtejapeddi@gmail.com>
2025-02-08 22:09:04 +0530

    Add files via upload

--- Square_And_Cubic_Sum.cu
+++ Square_And_Cubic_Sum.cu
@@ -7,24 +7,24 @@ __global__ void dkernel(int *a,int *b,int *c)
 }
 
   int main(){
-    int i,a[N],b[N],c[N],*a_gpu[N],*b_gpu[N],*c_gpu[N];
-    for (i =1;i<N;i++)
+    int i,a[N],b[N],c[N],*a_gpu,*b_gpu,*c_gpu;
+    for (i =0;i<N;i++)
     {
        a[i] = i*i;
     }
-        for (i =1;i<N;i++)
+        for (i =0;i<N;i++)
     {
        b[i] = i*i*i;
     }
-    cudamalloc(&a_gpu,N*sizeof(int));
-    cudamalloc(&b_gpu,N*sizeof(int));
-    cudamalloc(&c_gpu,N*sizeof(int));
+    cudaMalloc(&a_gpu,N*sizeof(int));
+    cudaMalloc(&b_gpu,N*sizeof(int));
+    cudaMalloc(&c_gpu,N*sizeof(int));
     cudaMemcpy(a_gpu,a,N*sizeof(int),cudaMemcpyHostToDevice);
     cudaMemcpy(b_gpu,b,N*sizeof(int),cudaMemcpyHostToDevice);
     dkernel<<<1,N>>>(a_gpu,b_gpu,c_gpu);
     cudaMemcpy(c,c_gpu,N*sizeof(int),cudaMemcpyDeviceToHost);
-    for (i=1;i<N;i++)
+    for (i=0;i<N;i++)
     {
-      printf("%d",c[i]);
+      printf("%d\n",c[i]);
     }
   }
