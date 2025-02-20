dac3b87ca47d3b7b5bbee78cdbb5386d784efff1
HarishTeja Peddi <harishtejapeddi@gmail.com>
2025-02-20 15:55:33 +0530

    Add files via upload

---
+++ matrix_squaring_v1.cu
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<cuda.h>
+#define N 4
+__global__ void dkernel(unsigned *matrix,unsigned *result){
+  unsigned idx = blockIdx.x*blockDim.x+threadIdx.x;
+  
+  //for (unsigned ii = 0; ii < N; ++ii) 
+       for (unsigned jj = 0; jj < N; ++jj)
+       {
+         for (unsigned kk = 0; kk < N; ++kk) {
+           result[idx * N + jj] +=
+           matrix[idx * N + kk] * matrix[kk * N + jj];
+         }
+       }
+}
+int main() {
+  unsigned *matrix,*result;
+  unsigned *hmatrix;
+  int i,j;
+  cudaMalloc(&matrix,N*N*sizeof(unsigned));
+  cudaMalloc(&result,N*N*sizeof(unsigned));
+  hmatrix = (unsigned*)malloc(N*N*sizeof(unsigned));
+  for(i=0;i<N;i++){
+    for(j=0;j<N;j++){
+      hmatrix[i*N+j] = i*N+j;
+    }
+  }
+  cudaMemcpy(matrix,hmatrix,N*N*sizeof(unsigned),cudaMemcpyHostToDevice);
+  dkernel<<< 1,N >>>(matrix,result);
+
+  cudaMemcpy(hmatrix,result,N*N*sizeof(unsigned),cudaMemcpyDeviceToHost);
+  for (i=0;i<N;i++){
+    for(j=0;j<N;j++){
+      printf("%d ",hmatrix[i*N+j]);
+    }
+    printf("\n");
+  }
+
+}
