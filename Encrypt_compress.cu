9d8d9f7e2db9f714d723405f79526907dcb493af
HarishTeja Peddi <harishtejapeddi@gmail.com>
2025-03-04 22:40:26 +0530

    Add files via upload

--- Encrypt_compress.cu
+++ Encrypt_compress.cu
@@ -1,32 +1,40 @@
 #include<stdio.h>
 #include<cuda.h>
 
-__global__ void dkernel(unsigned *arr,int N)
+__global__ void dkernel(unsigned *arr,unsigned *ans,int N,int* i)
 {
   unsigned id = threadIdx.x;
   unsigned count = 1;
-  //if (id<N)
-  //{
-   while (arr[id] = arr[id+1])
+  if (id<N)
+  {
+   if (arr[id] == arr[id+1] && arr[id]!=arr[id-1])
    {
-    count +=1;
-    id +=1;
+    while(arr[id] == arr[id+1])
+    {count +=1;
+    id +=1;}
+    ans[i] = count;
+    i++;
    }
-   arr[threadIdx.x] = count;
+   
   }
-  //}
+  }
 int main()
 {
-  unsigned *arr;
-  int N;
+  unsigned *arr,*dans,*di;
+  int N,i=0;
   int arrh[] = {0,0,0,1,1,0,1,0,0,0,1,0,0,0,1,1,1,1,0,1,1,1,0,1,0,0,0,1};
+ // int ans[] = {0,0,0,1,1,0,1,0,0,0,1,0,0,0,1,1,1,1,0,1,1,1,0,1,0,0,0,1};
   N=sizeof(arrh)/sizeof(arrh[0]);
 
-  cudaMalloc(&arr,N*sizeof(unsigned)); 
+  cudaMalloc(&arr,N*sizeof(unsigned));
   cudaMemcpy(arr,arrh,sizeof(arrh),cudaMemcpyHostToDevice);
+  cudaMalloc(&dans,N*sizeof(unsigned));
+  cudaMalloc(&di,1*sizeof(unsigned));
+  cudaMemcpy(di,&i,sizeof(unsigned),cudaMemcpyHostToDevice);
+  //cudaMemcpy(dans,ans,sizeof(ans),cudaMemcpyHostToDevice);
 
-  dkernel<<< 1,N >>>(arr,N);
-  cudaMemcpy(arrh,arr,N*sizeof(unsigned),cudaMemcpyDeviceToHost);
+  dkernel<<< 1,N >>>(arr,ans,N,di);
+  cudaMemcpy(arrh,ans,N*sizeof(unsigned),cudaMemcpyDeviceToHost);
 
   for (int i=0;i<N;i++)
   {
